d10e79305292102ec2b80257815c375ae49a78ef
jvoon <119896703+jvoon@users.noreply.github.com>
2025-11-12 23:20:30 -0500

    Update template_stats.js

--- template_stats.js
+++ template_stats.js
@@ -1,23 +1,51 @@
+//cleaning up some of the tutorial comments stuff to put my reflection at the very top!
+
+// Refection: 
+//   I chose to use the Charts.js enhancement because it felt like a reasonable 'upgrade' given my previous
+// adaption of adding orange bars to the stats. This gets it done so much faster (and more aesthetically) and shows 
+// my roommate that Animal Crossing is interestingly Nintendo's current favorite child (not that I would blame them).
+
+//   In terms of addressing user problems, having a visual graph along with percentages helps to outline the degree of
+// differences in metrics between elements (for example, I noticed there were a lot of AC amiibos, but I didn't realize 
+// how many MORE AC amiibos there are in relative comparison to others, i thought there would be a lot more Mario or LoZ)
+// I opted for different views depending on the density of how many elments are being compared within a single category.
+
+//   A challenge I encountered among the many was actually limiting the rescale of the pie chart while not affectng the bar.
+// Because I set it so that the chart was determined long before the orientation, I for some reason had difficulty 
+// incorporating restrictions on different tpyes since I was used to more universal settings. But eventually I slapped in
+// an if-else statement to manage the style.
+
+//   This enhacement would hopefully give a more well-informed overview on what data is presernt in this API. Noting the
+// most popular types of collectables, the most popular genres of collectables, and more. Also, it mgiht be a bit fun to 
+// click between the different graphs! 
+
+//   If I had more time, I would definitely reexplore the if-else I mentioned earlier and figure out how to properly 
+// incorproate it into the CSS...
+
 function showStats(data) {
   const total = data.length;
   const calcPercent = (value) => ((value / total) * 100).toFixed(1);
 
-  // ----- Summaries -----
+  //the summaries
+  //by type
   const typeSummary = data.reduce((acc, item) => {
     const type = item.type || "Unknown";
     acc[type] = (acc[type] || 0) + 1;
     return acc;
   }, {});
+  //by amiibo series
   const seriesSummary = data.reduce((acc, item) => {
     const series = item.amiiboSeries || "Unknown";
     acc[series] = (acc[series] || 0) + 1;
     return acc;
   }, {});
+  //by game series
   const gameSummary = data.reduce((acc, item) => {
     const game = item.gameSeries || "Unknown";
     acc[game] = (acc[game] || 0) + 1;
     return acc;
   }, {});
+  //by region
   const regions = ["na", "jp", "eu", "au"];
   const regionSummary = regions.reduce((acc, r) => {
     acc[r.toUpperCase()] = data.filter(item => item.release?.[r]).length;
@@ -31,7 +59,8 @@ function showStats(data) {
     region: { title: "Amiibo Distribution by Region", data: regionSummary, chartType: "pie" },
   };
 
-  // ----- HTML OUTPUT -----
+  //html output
+  //since i'm using charts.js, i don't really need the orange bars anymore (goodbye orange bars~!)
   const html = `
     <h1 style="text-align:center;">Total Amiibos: ${total}</h1>
     <div class="chart-container" style="width:70%;margin:2em auto;">
@@ -70,7 +99,7 @@ function showStats(data) {
     </div>
   `;
 
-  // ----- Chart Setup -----
+  //chart setup 
   setTimeout(() => {
     let currentChart = null;
 
@@ -81,7 +110,7 @@ function showStats(data) {
     }
 
 
-    // Utility: Render a chart from one of the summary objects
+    //render a chart from a determined summary object
     const renderChart = (summaryKey) => {
   const { title, data, chartType } = summaries[summaryKey];
   const labels = Object.keys(data);
@@ -89,7 +118,7 @@ function showStats(data) {
 
   if (currentChart) currentChart.destroy();
 
-  // Set max height only for pie charts
+  //set max height only for pie charts
   if (chartType === 'pie') {
     ctx.style.maxHeight = '350px'; // limit pie chart height
   } else {
@@ -109,6 +138,8 @@ function showStats(data) {
         ],
       }],
     },
+    //set a little legend for pie charts (too many options for bar)
+    //set xy axis for bar
     options: {
       responsive: true,
       animation: { duration: 600 },
@@ -125,15 +156,15 @@ function showStats(data) {
 };
 
 
-    // Default chart (start with type)
+    //default chart (start with type)
     renderChart("type");
 
-    // Click listeners (instead of hover)
+    //click listeners (i tried hover. then i cried)
     document.querySelectorAll(".stat-box").forEach((box) => {
       const chartKey = box.dataset.chart;
       box.addEventListener("click", () => {
         renderChart(chartKey);
-        // Optional: add a little visual cue for active section
+        //add a little visual cue for active section
         document.querySelectorAll(".stat-box").forEach(b => b.classList.remove("active"));
         box.classList.add("active");
       });
